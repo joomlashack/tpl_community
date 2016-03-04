071c49e762c1cc592f63f6d7adc48fab2fbb23f4
jmarkos <markos@joomlashack.com>
2016-03-03 20:07:54 -0600

    update search override

--- code/html/com_search/search/default_form.php
+++ code/html/com_search/search/default_form.php
@@ -1,71 +1,80 @@
-<?php defined('_JEXEC') or die('Restricted access'); ?>
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_search
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
-<form id="searchForm" action="<?php echo JRoute::_( 'index.php?option=com_search' );?>" method="post">
-	<div class="contentpaneopen<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-		<div>
-				<label for="search_searchword">
-					<?php echo JText::_( 'Search Keyword' ); ?>:
-				</label>
+defined('_JEXEC') or die;
+
+JHtml::_('bootstrap.tooltip');
+
+$lang = JFactory::getLanguage();
+$upper_limit = $lang->getUpperLimitSearchWord();
+?>
+<form id="searchForm" action="<?php echo JRoute::_('index.php?option=com_search');?>" method="post">
+
+	<div class="btn-toolbar">
+		<div class="btn-group pull-left">
+			<input type="text" name="searchword" placeholder="<?php echo JText::_('COM_SEARCH_SEARCH_KEYWORD'); ?>" id="search-searchword" size="30" maxlength="<?php echo $upper_limit; ?>" value="<?php echo $this->escape($this->origkeyword); ?>" class="inputbox" />
 		</div>
-		<div>
-				<input type="text" name="searchword" id="search_searchword" size="30" maxlength="20" value="<?php echo $this->escape($this->searchword); ?>" class="inputbox" /><button name="Search" onclick="this.form.submit()" class="button"><?php echo JText::_( 'Search' );?></button>
-				
+		<div class="btn-group pull-left">
+			<button name="Search" onclick="this.form.submit()" class="btn hasTooltip" title="<?php echo JHtml::tooltipText('COM_SEARCH_SEARCH');?>"><span class="icon-search"></span></button>
 		</div>
-		<div>
-		</div>
-		<div>
+		<input type="hidden" name="task" value="search" />
+		<div class="clearfix"></div>
+	</div>
+
+	<div class="searchintro<?php echo $this->params->get('pageclass_sfx'); ?>">
+		<?php if (!empty($this->searchword)):?>
+		<p><?php echo JText::plural('COM_SEARCH_SEARCH_KEYWORD_N_RESULTS', '<span class="badge badge-info">' . $this->total . '</span>');?></p>
+		<?php endif;?>
+	</div>
+
+	<?php if ($this->params->get('search_phrases', 1)) : ?>
+		<fieldset class="phrases">
+			<legend><?php echo JText::_('COM_SEARCH_FOR');?>
+			</legend>
+				<div class="phrases-box">
 				<?php echo $this->lists['searchphrase']; ?>
-		</div>
-		<div>
-				<label for="ordering">
-					<?php echo JText::_( 'Ordering' );?>:
+				</div>
+				<div class="ordering-box">
+				<label for="ordering" class="ordering">
+					<?php echo JText::_('COM_SEARCH_ORDERING');?>
 				</label>
 				<?php echo $this->lists['ordering'];?>
-		</div>
-	</div>
-	<?php if ($this->params->get( 'search_areas', 1 )) : ?>
-		<div>
-		<?php echo JText::_( 'Search Only' );?>:
-		</div>
-		<?php foreach ($this->searchareas['search'] as $val => $txt) :
-			$checked = is_array( $this->searchareas['active'] ) && in_array( $val, $this->searchareas['active'] ) ? 'checked="checked"' : '';
-		?>
-		<ins>
-		<input type="checkbox" name="areas[]" value="<?php echo $val;?>" id="area_<?php echo $val;?>" <?php echo $checked;?> />
-			<label for="area_<?php echo $val;?>">
-				<?php echo JText::_($txt); ?>
-			</label>
-		</ins>
-		<?php endforeach; ?>
+				</div>
+		</fieldset>
 	<?php endif; ?>
 
+	<?php if ($this->params->get('search_areas', 1)) : ?>
+		<fieldset class="only">
+			<legend><?php echo JText::_('COM_SEARCH_SEARCH_ONLY');?></legend>
+			<?php foreach ($this->searchareas['search'] as $val => $txt) :
+				$checked = is_array($this->searchareas['active']) && in_array($val, $this->searchareas['active']) ? 'checked="checked"' : '';
+			?>
+			<label for="area-<?php echo $val;?>" class="checkbox">
+				<input type="checkbox" name="areas[]" value="<?php echo $val;?>" id="area-<?php echo $val;?>" <?php echo $checked;?> >
+				<?php echo JText::_($txt); ?>
+			</label>
+			<?php endforeach; ?>
+		</fieldset>
+	<?php endif; ?>
 
-	<div class="searchintro<?php echo $this->params->get( 'pageclass_sfx' ); ?>">
-		<div>
-			<br />
-			<?php echo JText::_( 'Search Keyword' ) .' <b>'. $this->escape($this->searchword) .'</b>'; ?>
-		</div>
-		<div>
-			<br />
-			<?php echo $this->result; ?>
-		</div>
-</div>
+<?php if ($this->total > 0) : ?>
 
-<?php if($this->total > 0) : ?>
-<div style="margin:0px auto;">
-	<div style="float: right;">
+	<div class="form-limit">
 		<label for="limit">
-			<?php echo JText::_( 'Display Num' ); ?>
+			<?php echo JText::_('JGLOBAL_DISPLAY_NUM'); ?>
 		</label>
-		<?php echo $this->pagination->getLimitBox( ); ?>
+		<?php echo $this->pagination->getLimitBox(); ?>
 	</div>
-	<div>
+<p class="counter">
 		<?php echo $this->pagination->getPagesCounter(); ?>
-	</div>
-</div>
+	</p>
+
 <?php endif; ?>
-<div>
-<input type="hidden" name="task"   value="search" />
-</div>
+
 </form>
-<div class="clear"></div>
