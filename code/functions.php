d0a98cc4aa3d6dd01b590bfec9b5736d012f3bf1
Valentín <htmgarcia@gmail.com>
2016-08-08 17:53:58 -0500

    Clean structure

---
+++ code/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     Community
+ * @subpackage  Template File
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Joomlashack. Meritage Assets.  All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
